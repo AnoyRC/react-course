c12d8a196d3e20d9edb742951eefeec053f8d77a
AnoyRC <anoyroyc3545@gmail.com>
2022-03-17 12:00:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unreachable */
+
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+  <div>
+    <h1>My Todos</h1>
+    <Todo text='Learn React' />    
+    <Todo text='Master React' />  
+    <Todo text='Explore the full react course' /> 
+  </div>
+  );
+}
+export default App;
