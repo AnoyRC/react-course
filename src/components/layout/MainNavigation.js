79945f4c5354a670f7d69c0bca8b06b860acb51a
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2022-03-23 23:00:11 +0530

    Meetup List

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -12,7 +12,7 @@ function MainNavigation(){
                        <Link to='/'>All Meetup</Link>
                    </li>
                    <li>
-                       <Link to='/new-meetup'>Add New Meetup</Link>
+                       <Link to='/new-meetups'>Add New Meetup</Link>
                    </li>
                    <li>
                        <Link to='/favorites'>My Favorites</Link>
---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,11 @@
+import MeetupItem from "./MeetupItem";
+import classes from "./MeetupList.module.css"
+
+function MeetupList(props){
+   return<ul className={classes.list}>
+       {props.meetup.map(meetup => <MeetupItem key={meetup.id} id={meetup.id} image={meetup.image} title ={meetup.title}
+       address={meetup.address} description = {meetup.description}/>)}
+   </ul>
+}
+
+export default MeetupList;
