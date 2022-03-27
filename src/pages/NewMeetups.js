a827514d6067c0784809df34a898cb8886739667
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2022-03-28 00:34:23 +0530

    Post Request in Backnd

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,22 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetups(){
+   function addMeetupHandler(meetupData){
+       fetch(
+           'https://react-getting-started-91f70-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+           {
+               method: 'POST',
+               body: JSON.stringify(meetupData),
+               headers:{
+                   'Content-Type' : 'application/json'
+               }
+           }
+        );
+   }
    return(
        <section>
            <h1>Add New Meetup</h1>
-           <NewMeetupForm />
+           <NewMeetupForm onAddMeetup={addMeetupHandler} />
        </section>
    );
 }
