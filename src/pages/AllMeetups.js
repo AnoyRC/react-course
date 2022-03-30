72542ba2ecb034232c93cd24dad4c94f3291a9b3
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2022-03-30 15:42:39 +0530

    CORS fix + ContextIntro

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -7,27 +7,30 @@ function AllMeetups(){
     const [isLoading, setIsLoading] = useState(true);
     const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
       setIsLoading(true);
-      fetch('https://react-getting-started-91f70-default-rtdb.asia-southeast1.firebasedatabase.app/meetups'
-      ).then(response=> {
-        return response.json();}
-      ).then(data=>{
-        const meetups = [];
-
-        for(const key in data){
-          const meetup = {
-            id:key,
-            ...data[key]
-          };
-
-          meetups.push(meetup);
-        }
-
-        setIsLoading(false);
-        setLoadedMeetups(meetups);
-      });
-    },[]);
+      fetch(
+        'https://react-getting-started-91f70-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json'
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const meetups = [];
+  
+          for (const key in data) {
+            const meetup = {
+              id: key,
+              ...data[key]
+            };
+  
+            meetups.push(meetup);
+          }
+  
+          setIsLoading(false);
+          setLoadedMeetups(meetups);
+        });
+    }, []);
 
     
 
