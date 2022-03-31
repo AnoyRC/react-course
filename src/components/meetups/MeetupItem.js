2580735ffb8cb5b7826f7ce471b1816cf217dcbb
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2022-03-31 23:47:56 +0530

    Context

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,7 +1,27 @@
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-context";
 import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props){
+    const favoritesCtx = useContext(FavoritesContext);
+
+    const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+    function toggleFavoriteStatusHandler(){
+        if(itemIsFavorite){
+            favoritesCtx.removeFavorite(props.id);
+        }
+        else{
+            favoritesCtx.addFavorite({
+                id : props.id,
+                title : props.title,
+                address : props.address,
+                description : props.description,
+                image : props.image
+            });
+        }
+    }
    return <li className={classes.item}>
        <Card>
        <div className={classes.image}>
@@ -13,7 +33,7 @@ function MeetupItem(props){
            <p>{props.description}</p>
        </div>
        <div className={classes.actions}>
-           <button>To Favorites</button>
+           <button onClick={toggleFavoriteStatusHandler}>{itemIsFavorite ? "Remove from Favorites" : "To Favorites"}</button>
        </div>
        </Card>
    </li>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { FavoritesContextProvider } from './store/favorites-context';
+
 
 ReactDOM.render(
+  <FavoritesContextProvider>
   <BrowserRouter>
     <App/>
-  </BrowserRouter>,
+  </BrowserRouter>
+  </FavoritesContextProvider>,
   document.getElementById('root')
 );
 
